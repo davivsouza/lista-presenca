50ad76c62c722cdb8348657c87c3c5f8a8eaf63a
davivsouza <davivasconcelossouza7@gmail.com>
2022-04-12 19:32:13 -0300

    passando projeto pra typescript

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Home } from './pages/Home'
-import { About } from './pages/About'
 import "./styles/global.css"
 
 ReactDOM.render(
   <React.StrictMode>
     <Home />
-    {/* <About /> */}
   </React.StrictMode>,
   document.getElementById('root')
 )
--- src/pages/About/index.jsx
+++
@@ -1,9 +0,0 @@
-import styles from"./styles.module.css"
-
-export function About(){
-  return(
-    <main className={styles.container}>
-      <h1>About Me</h1>
-    </main>
-  )
-}
